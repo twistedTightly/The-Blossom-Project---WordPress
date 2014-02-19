5b001560e57c9d97eb7130222186dc9fdf1c5501
twistedTightly <mrauh1@nd.edu>
2014-02-19 01:28:53 -0500

    Logo replaced text for page header with corresponding CSS

--- wp-content/themes/blossom-girls/header.php
+++ wp-content/themes/blossom-girls/header.php
@@ -57,7 +57,9 @@
 				</aside>
 
 		        <hgroup>
-		         	<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		         	<h1 class="site-title aligncenter"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+		         		<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/logos/logo_UPDATED_green.png">
+		         	</a></h1>
     				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		        </hgroup>
 		        <nav role="navigation" class="site-navigation main-navigation">
