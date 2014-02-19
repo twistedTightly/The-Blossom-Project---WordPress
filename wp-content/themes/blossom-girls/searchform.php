189c68c7439666b3a957cef241d717063bc002be
twistedTightly <mrauh1@nd.edu>
2014-02-19 01:26:24 -0500

    search placeholder text change

--- wp-content/themes/blossom-girls/searchform.php
+++ wp-content/themes/blossom-girls/searchform.php
@@ -7,7 +7,7 @@
  */
 ?>
     <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-        <label for="s" class="assistive-text"><?php _e( 'Search', 'BlossomGirls' ); ?></label>
-        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'BlossomGirls' ); ?>" />
+        <label for="s" class="assistive-text"><?php _e( 'search', 'BlossomGirls' ); ?></label>
+        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'search', 'BlossomGirls' ); ?>" />
         <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'BlossomGirls' ); ?>" />
     </form>
