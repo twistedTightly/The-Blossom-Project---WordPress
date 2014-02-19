0f25c9076ee931ee21513ed5dd5690046490b24e
twistedTightly <mrauh1@nd.edu>
2014-02-19 01:26:58 -0500

    Attempt to move search from sidebar to header

--- wp-content/themes/blossom-girls/sidebar.php
+++ wp-content/themes/blossom-girls/sidebar.php
@@ -10,10 +10,6 @@
     <?php do_action( 'before_sidebar' ); ?>
     <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
  
-        <aside id="search" class="widget widget_search">
-            <?php get_search_form(); ?>
-        </aside>
- 
         <aside id="archives" class="widget">
             <h1 class="widget-title"><?php _e( 'Archives', 'BlossomGirls' ); ?></h1>
             <ul>
