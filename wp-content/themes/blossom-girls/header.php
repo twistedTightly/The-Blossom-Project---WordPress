63eab1b79cff7c1e2be3d701185a7dbab4d06261
twistedTightly <mrauh1@nd.edu>
2014-03-14 09:53:51 +0100

    Updated Blossom logo (color change)

--- wp-content/themes/blossom-girls/header.php
+++ wp-content/themes/blossom-girls/header.php
@@ -65,7 +65,7 @@
 
 		        <hgroup>
 		         	<h1 class="site-title aligncenter"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-		         		<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/logos/logo_UPDATED_green.png">
+		         		<img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/images/logos/logo_FINAL_3_2.png">
 		         	</a></h1>
     				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		        </hgroup>
