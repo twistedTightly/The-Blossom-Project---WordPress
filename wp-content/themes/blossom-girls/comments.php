ecee75b34b82746678ab6700dbc5279ccd14f659
twistedTightly <mrauh1@nd.edu>
2014-04-01 17:02:14 -0400

    Initial comments HTML restructuring and CSS styling

--- wp-content/themes/blossom-girls/comments.php
+++ wp-content/themes/blossom-girls/comments.php
@@ -23,13 +23,28 @@
 ?>
  
     <div id="comments" class="comments-area">
- 
+
+    <?php 
+        $comments_args = array(
+            // change the title of send button 
+            'label_submit'=>'Submit',
+            // change the title of the reply section
+            'title_reply'=>'',
+            // remove "Text or HTML to be displayed after the set of comment fields"
+            'comment_notes_after' => '',
+            // redefine your own textarea (the comment body)
+            'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Have a comment or question? Submit it below:', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+        );
+
+        comment_form($comments_args);
+    ?>
+
     <?php // You can start editing here -- including this comment! ?>
  
     <?php if ( have_comments() ) : ?>
         <h2 class="comments-title">
             <?php
-                printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'BlossomGirls' ),
+                printf( _n( 'Comments on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s:&rdquo;', get_comments_number(), 'BlossomGirls' ),
                     number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
             ?>
         </h2>
@@ -71,6 +86,6 @@
         <p class="nocomments"><?php _e( 'Comments are closed.', 'BlossomGirls' ); ?></p>
     <?php endif; ?>
  
-    <?php comment_form(); ?>
+    <?php // comments form was here before ?>
  
 </div><!-- #comments .comments-area -->
