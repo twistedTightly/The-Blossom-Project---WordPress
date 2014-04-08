7969f6908d2ef9a260a915794347d642286aa961
twistedTightly <mrauh1@nd.edu>
2014-04-08 16:29:22 -0400

    Page content loop rewritten
    Now accommodates Girl Talk posts

--- wp-content/themes/blossom-girls/page.php
+++ wp-content/themes/blossom-girls/page.php
@@ -21,18 +21,59 @@ get_header(); ?>
         <?php endif; ?>
         <!--div id="primary" class="content-area"-->
             <div id="content" class="site-content" role="main">
+
+                <?php if ( is_page('Girl Talk') ) : // For Girl Talk... ?>
+
+                    <?php $girl_talk_posts = new WP_Query('cat=girl-talk'); // Query for all posts written in that category ?>
+                    <?php if ( $girl_talk_posts->have_posts() ) : ?>
+
+                        <?php /* Start the Loop */ ?> 
+                        <?php $loopcounter = 0; // Initialize counter to find first post ?>
+                        <?php while ( $girl_talk_posts->have_posts() ) : $girl_talk_posts->the_post(); ?>
+                            <?php
+                                /* Include the Post-Format-specific template for the content.
+                                * If you want to overload this in a child theme then include a file
+                                * called content-___.php (where ___ is the Post Format name) and    that will be used instead.
+                                */ ?>
+                                <?php $loopcounter++; ?>
+                                <?php if ( $loopcounter== 1 ) : ?>
+
+                                    <div id="girl-talk-first-post">
+                                        <?php get_template_part( 'content', 'girl-talk' ); // Specific template for most recent Girl Talk post when on Girl Talk page itself ?>
+                                    </div>
+
+                                    <?php while ( have_posts() ) : the_post(); // Get 'Girl Talk' page content, skipping most recent 'Girl Talk' post ?>
+                                        <?php get_template_part( 'content', 'page' ); // Their regular content is shown instead ?>
+                                    <?php endwhile; // end of the loop. ?>
+
+                                <?php else : ?>
+
+                                    <?php get_template_part( 'content', get_post_format() ); // Get posts in 'Girl Talk' category ?>
+
+                                <?php endif; ?>
+
+                        <?php endwhile; ?>
+                        
+                        <?php BlossomGirls_content_nav( 'nav-below' ); ?>
+                    <?php else : ?>
+                        <?php get_template_part( 'no-results', 'index' ); ?>
+                    <?php endif; ?>
+
+                <?php else: ?>
+
+                    <?php while ( have_posts() ) : the_post(); // All other pages are not treated like blogs ?>
+
+                        <?php get_template_part( 'content', 'page' ); // Their regular content is shown instead ?>
+     
+                        <?php if (is_page( array('About Us','Connect','Girl Talk', 'Get Informed', 'Shop', 'Donate', 'Home', 'Coming Soon') )) : ?>
+                        <?php else : ?>
+                            <?php comments_template( '', true ); ?>
+                        <?php endif; ?>
  
-                <?php while ( have_posts() ) : the_post(); ?>
- 
-                    <?php get_template_part( 'content', 'page' ); ?>
- 
- 					<?php if (is_page( array('About Us','Connect','Girl Talk', 'Get Informed', 'Shop', 'Donate', 'Home', 'Coming Soon') )) : ?>
-					<?php else : ?>
-                    	<?php comments_template( '', true ); ?>
-					<?php endif; ?>
- 
-                <?php endwhile; // end of the loop. ?>
- 
+                    <?php endwhile; // end of the loop. ?>
+
+                <?php endif; ?>
+                
             </div><!-- #content .site-content -->
         </div><!-- #primary .content-area -->
  
