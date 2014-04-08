7bd022b7b51d0814f6dcf0d29fea4626ede788c8
twistedTightly <mrauh1@nd.edu>
2014-04-08 16:25:37 -0400

    Query added so only non-girl talk posts show in blog

--- wp-content/themes/blossom-girls/index.php
+++ wp-content/themes/blossom-girls/index.php
@@ -19,24 +19,26 @@ get_header(); ?>
 
 <div id="primary" class="content-area alignleft">
     <div id="content" class="site-content" role="main">
-    	<?php if ( have_posts() ) : ?>
-	    	<?php //BlossomGirls_content_nav( 'nav-above' ); ?>
+    	<?php /* Get all posts that are not for Girl Talk page (not category 5) */ ?>
+		<?php query_posts('cat=-5'); ?>
+	    	<?php if ( have_posts() ) : ?>
+		    	<?php //BlossomGirls_content_nav( 'nav-above' ); ?>
 
-		    <?php /* Start the Loop */ ?>
-		    <?php while ( have_posts() ) : the_post(); ?>
-		        <?php
-				    /* Include the Post-Format-specific template for the content.
-				    * If you want to overload this in a child theme then include a file
-				    * called content-___.php (where ___ is the Post Format name) and    that will be used instead.
-				    */
-				    get_template_part( 'content', get_post_format() );
-			    ?>
-	    	<?php endwhile; ?>
+			    <?php /* Start the Loop */ ?>
+			    <?php while ( have_posts() ) : the_post(); ?>
+			        <?php
+					    /* Include the Post-Format-specific template for the content.
+					    * If you want to overload this in a child theme then include a file
+					    * called content-___.php (where ___ is the Post Format name) and    that will be used instead.
+					    */
+					    	get_template_part( 'content', get_post_format() );
+				    ?>
+		    	<?php endwhile; ?>
 
-	    	<?php BlossomGirls_content_nav( 'nav-below' ); ?>
-	    <?php else : ?>
-     		<?php get_template_part( 'no-results', 'index' ); ?>
-		<?php endif; ?>
+		    	<?php BlossomGirls_content_nav( 'nav-below' ); ?>
+		    <?php else : ?>
+	     		<?php get_template_part( 'no-results', 'index' ); ?>
+			<?php endif; ?>
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
 <?php get_sidebar(); ?>
